7093b8e5f191463812872063a7a5c1e570f9ab23
CherishYii <cherish@cherish.pw>
2020-10-26 10:46:37 +0800

    01_part1

---
+++ day01_component/src/App2.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+// 导入子组件
+import Item from './Components/Item'
+
+class App extends Component {
+    render(){
+        return <Item></Item>
+    }
+}
+
+export default App;
